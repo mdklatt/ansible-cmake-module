56dc1aaf0a1ba6e437e8c8b0817d0d5aee78330b
Michael Klatt <mdklatt@ou.edu>
2016-06-03 00:13:25 -0500

    added module argument specification

--- lib/cmake.py
+++ lib/cmake.py
@@ -9,12 +9,27 @@ from ansible.module_utils.basic import AnsibleModule
 __all__ = "main",
 
 
+_ARGS_SPEC = {
+    # MUST use list for choices.
+    "build_type": {
+        "default": "Debug",
+        "choices": ["Debug", "Release", "RelWithDebInfo", "MinSizeRel"],
+    },
+    "binary_dir": {},
+    "source_dir": {},
+    "target": {},
+    "executable": {"default": "cmake"},
+    "clean_first": {"default": False, "type": "bool"},
+    "vars": {"type": "dict"},
+}
+
+
 def main():
     """ Execute the module.
 
     """
-    module = AnsibleModule({})
-    module.fail_json(msg="not implemented")  # calls exit(1)
+    module = AnsibleModule(_ARGS_SPEC, supports_check_mode=False)
+    module.exit_json(changed=False, **module.params)  # calls sys.exit(0)
 
 
 # Make the module executable.
--- test/test_cmake.py
+++ test/test_cmake.py
@@ -8,12 +8,15 @@ run with `--ansible-host-pattern=localhost`.
 import pytest
 
 
-def test_cmake(ansible_module):
-    """ Test the cmake module.
+@pytest.mark.parametrize("build", ("Debug", "Release", "RelWithDebInfo", "MinSizeRel"))
+def test_args(ansible_module, build):
+    """ Test module arguments.
 
     """
-    result = ansible_module.cmake()
-    assert u"not implemented" == result["localhost"]["msg"]
+    # For now, this just tests that each build type is accepted
+    params = {"build_type": build}
+    result = ansible_module.cmake(**params)
+    assert build == result["localhost"]["build_type"]
     return
 
 
