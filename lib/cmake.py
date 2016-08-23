8eca78eb4b58eba57febfcbb37229f527589e83d
Michael Klatt <mdklatt@ou.edu>
2016-08-23 17:03:10 -0500

    v0.1.1

--- lib/cmake.py
+++ lib/cmake.py
@@ -17,7 +17,7 @@ from ansible.module_utils.basic import AnsibleModule
 __all__ = "main",
 
 
-__version__ = "0.1.1dev2"  # PEP 0440 with Semantic Versioning
+__version__ = "0.1.1"  # PEP 0440 with Semantic Versioning
 
 
 DOCUMENTATION = """
