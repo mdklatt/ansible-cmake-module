108a3d85f730ed14fb37242f5e7fcd7c350f8ce5
Michael Klatt <mdklatt@alumni.ou.edu>
2020-12-09 12:29:57 -0600

    Tweaked test/test_module.py

--- test/test_module.py
+++ test/test_module.py
@@ -77,5 +77,4 @@ def test_clean(tmpdir, ansible_module):
 # Make the module executable.
 
 if __name__ == "__main__":
-    raise SystemExit(pytest.main([__file__]))
-
+    raise exit(pytest.main([__file__]))
